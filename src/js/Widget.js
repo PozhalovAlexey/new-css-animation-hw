64e133a4b181d258954f54cc405063abcaaca649
Alexey Pozhalov <pozhalov7@mail.ru>
2024-04-02 21:16:42 +0300

    готовый проект

---
+++ src/js/Widget.js
@@ -0,0 +1,32 @@
+export default class Widget {
+    collapsing() {
+        const container = document.getElementById('widget');
+        const wrapper = document.createElement('button');
+
+        wrapper.innerHTML = `
+      <button class="btn-collapse">Collapse</button>
+      <div class="text-container">
+        <div class="text-collapse">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
+      <div class="copy"></div>
+    `;
+
+        container.insertAdjacentHTML('beforeend', wrapper.outerHTML);
+
+        const btn = document.querySelector('.btn-collapse');
+        const textContainer = document.querySelector('.text-container');
+
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            btn.classList.toggle('collapsed');
+
+            if (btn.classList.contains('collapsed')) {
+                textContainer.classList.remove('show');
+            } else {
+                textContainer.classList.add('show');
+            }
+        });
+    }
+}
